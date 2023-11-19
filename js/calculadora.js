4c90e7d534d27dbf9891c1eb04dd89f4b61e8220
Maxxxm69 <maxmiguel004@gmail.com>
2023-11-19 15:21:45 -0600

    agregué espacios

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,6 +1,8 @@
 // los botones de la calculadora escriben en el display sus valores respectivos
 // Función para escribir en el contenedor
-//comentario2
+//comentario3
+
+
 function escribirEnContenedor(caracter) {
     let contenedor = document.getElementById("gauss");
     contenedor.value += caracter;
