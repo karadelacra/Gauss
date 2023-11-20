2f7ac317100ccc8b064bd54d96f5c05aab581c10
Karadelacra <padillakarel5@gmail.com>
2023-11-20 15:37:07 -0600

    new style and archive button

--- js/archivo2.js
+++ js/archivo2.js
@@ -2,24 +2,18 @@
 
 function save() {
   const content = document.getElementById('rawmatriz').value;
-  const content2 = document.getElementById('rawmatriz2').value;
-  const content3 = document.getElementById('rawmatriz3').value;
-
-  console.log(content);
-  console.log(content2);
-  console.log(content3);
+  if (content === '') {
+    return;
+  }
 
   const matrix = scanmatrix(content)
+  let n = matrix.length;
+  let m = matrix[0].length;
 
-  const n = matrix.length;
-  const m = matrix[0].length;
-
+  const blob = new Blob([content], { type: 'text/plain' });
   const link = document.createElement('a');
-  // juntar los datos de los resultados del programa separados con dos saltos de línea
-  const data = '✓ gausJordan \n' + content + '\n\n ✓ determinante \n' + content2 + '\n\n\n ✓ inversa \n' + content3;
-  const blob = new Blob([data], { type: 'text/plain' });
   link.href = URL.createObjectURL(blob);
-  link.download = `Matriz ${n}x${m}.txt`;
+  link.download = `gauss${n}x${m}.txt`;
 
   // Simular un clic en el enlace para iniciar la descarga
   link.click();
@@ -30,7 +24,6 @@ function save() {
     link.remove();
   }, 100);
 }
-
 
 function readtextfile() {
   const fileInput = document.getElementById("Carch").files[0];
@@ -80,6 +73,7 @@ function clearTable(table) {
     table.deleteRow(0);
   }
 }
+
 function mostrarImagenYDatos() {
   document.getElementById('imagenMostrada').src = '';
   document.getElementById('datosImagen').innerHTML = '';
@@ -100,27 +94,22 @@ function mostrarImagenYDatos() {
 
       reader.readAsText(archivo);
       return;
-    }
-    else {
-      // limpiar el textbox
+    } else {
       textbox.value = '';
       reader.onload = function (e) {
         imagenMostrada.src = e.target.result;
 
-        // Muestra datos de la imagen
         datosImagen.innerHTML = `
-        <p>Nombre: ${archivo.name}</p>
-        <p>Tipo: ${archivo.type}</p>
-        <p>Tamaño: ${archivo.size} bytes</p>
-      `;
+          <p>Nombre: ${archivo.name}</p>
+          <p>Tipo: ${archivo.type}</p>
+          <p>Tamaño: ${archivo.size} bytes</p>
+        `;
       };
     }
     reader.readAsDataURL(archivo);
 
-    // Muestra la sección de imagen y datos
     imagenContainer.style.display = 'block';
   } else {
-    // Oculta la sección de imagen y datos si no hay archivo seleccionado
     imagenContainer.style.display = 'none';
   }
 }
