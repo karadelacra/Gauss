d89994c23361b7fdc4f4d1b2d7fcb9837071a3b0
Dario Quinones <daqucr2@gmail.com>
2024-01-16 19:15:37 -0600

    Mover con flechas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var WIDTH = 3;
+var HEIGHT = 3;
+
 function calculartodo() {
     const textbox = document.getElementById("gauss");
     document.getElementById("resultados").classList.remove("hidden")
@@ -12,14 +15,45 @@ function calculartodo() {
 }
 
 
-function enter(){
-// Obtén el botón y añade un listener al evento "keypress" del documento
-document.addEventListener("keypress", function (event) {
-    // Si la combinación enter
-    if (event.keyCode == 13) {
-        document.getElementById("accion").click();
+function checkEvents(){
+document.addEventListener("keydown", function (event) {
+    // Si se pulsan las flechas, mover el foco a la celda correspondiente
+    const findNeighbors = (row, col) => {
+        row = parseInt(row);
+        col = parseInt(col);
+        const neighbors = [];
+        if (col === 0) neighbors.push(`cell-${row}-${HEIGHT - 1}`); // Arriba
+        else neighbors.push(`cell-${row}-${col - 1}`);
+        if (col === HEIGHT - 1) neighbors.push(`cell-${row}-0`); // Abajo
+        else neighbors.push(`cell-${row}-${col + 1}`);
+        if (row === 0) neighbors.push(`cell-${WIDTH - 1}-${col}`); // Izquierda
+        else neighbors.push(`cell-${row - 1}-${col}`);
+        if (row === WIDTH - 1) neighbors.push(`cell-0-${col}`); // Derecha
+        else neighbors.push(`cell-${row + 1}-${col}`);
+        return neighbors;
+    };
+    const cell = document.activeElement;
+    const [row, col] = cell.id.split("-").slice(1);
+    console.log(row, col);
+    const neighbors = findNeighbors(row, col);
+    switch (event.code) {
+        case "ArrowUp":
+            document.getElementById(neighbors[0]).focus();
+            break;
+        case "ArrowDown":
+            document.getElementById(neighbors[1]).focus();
+            break;
+        case "ArrowLeft":
+            document.getElementById(neighbors[2]).focus();
+            break;
+        case "ArrowRight":
+            document.getElementById(neighbors[3]).focus();
+            break;
+        case "Enter":
+            calculartodo();
+            break;
+        default:
+            console.log(event.code);
     }
 });
 }
-
-
